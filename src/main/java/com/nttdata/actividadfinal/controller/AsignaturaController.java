777fb68a63588af5541fe82ca8c5f0a8767dbf42
alexfandila <alexfandila.com>
2022-03-14 13:16:38 +0100

    Eliminar enlace asignaturas para consultar

--- src/main/java/com/nttdata/actividadfinal/controller/AsignaturaController.java
+++ src/main/java/com/nttdata/actividadfinal/controller/AsignaturaController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,6 +22,13 @@ public class AsignaturaController {
 
 	@Autowired
 	UsuarioService usuarioService;
+	
+	@GetMapping("/")
+	public String index(Model model) {
+		Usuario u = (Usuario) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		model.addAttribute("usuario", u);
+		return "index";
+	}
 
 	@PreAuthorize("hasRole('ROLE_ADMIN')")
 	@GetMapping("/asignaturas")
